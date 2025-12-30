a0593d6e93ca60686ffb87978657fb40804fc546
Aditya Vahlevy Nugraha <adityavahlevy1003@gmail.com>
2025-12-30 10:17:24 +0700

    chore(seo): install and configure unhead

--- src/main.js
+++ src/main.js
@@ -11,4 +11,8 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+import { createHead } from '@unhead/vue/client'
+const head = createHead()
+app.use(head)
+
 app.mount('#app')
